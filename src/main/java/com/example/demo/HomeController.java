c341972286a80b1e9f8b2f16dc9bae5302a94a7b
bmar3315 <isr0618@naver.com>
2020-12-14 15:03:44 +0900

    12/14 15

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,12 +1,21 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("/")
     public String index(){
+        logger.trace("Trace Level 테스트"); 
+        logger.debug("DEBUG Level 테스트"); 
+        logger.info("INFO Level 테스트"); 
+        logger.warn("Warn Level 테스트"); 
+        logger.error("ERROR Level 테스트");
         return "index";
     }
 }
